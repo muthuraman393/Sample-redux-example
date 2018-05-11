244c0f969288499ae9a69bbae9612424d2b5ab07
muthuraman393 <muthuraman393@gmail.com>
2018-05-11 19:10:59 +0530

    react redux example

---
+++ assets/index.js
@@ -0,0 +1,17 @@
+import {createStore} from 'redux';
+const initialState ={
+ count:3
+}
+const reducer = (state = initialState,action)=> {
+ console.log('reducer running',action);
+ switch(action){
+	case 'INCREMENT':
+	return Object.assign({},state,{count:state.count+1});
+    default: 
+	return state;
+ }
+ 
+ return state;
+}
+const store = createStore(reducer);
+export default store;
